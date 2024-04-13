77f9332a3f2050badf36ff75acbfa48ec5430ee8
gubbeul <108100435+gubbeul@users.noreply.github.com>
2024-04-13 09:58:40 +0900

    습도 센서를 활용한 수업

--- main.ts
+++ main.ts
@@ -4,7 +4,10 @@ basic.forever(function () {
     basic.showNumber(pins.analogReadPin(AnalogPin.P1))
     if (pins.analogReadPin(AnalogPin.P1) < 200) {
         basic.showIcon(IconNames.Angry)
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Baddy), music.PlaybackMode.UntilDone)
+        basic.pause(5000)
     } else {
-    	
+        basic.showIcon(IconNames.Heart)
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Ode), music.PlaybackMode.UntilDone)
     }
 })
